42f76bc5a3b313d6c8439fa2907bc5e2d77e4c4b
s-poon <stevenpoon12.03@gmail.com>
2022-04-09 21:20:06 -0600

    zip version

---
+++ Assignments/Assignment5/30094433 - Copy/edu/ucalgary/ensf409/VisitorParking.java
@@ -0,0 +1,110 @@
+package edu.ucalgary.ensf409;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Set;
+import java.util.regex.*;
+
+import javax.lang.model.util.ElementScanner6;
+
+
+public class VisitorParking {
+    // Member Variables
+    private static final String REGEX = "[a-zA-Z0-9 -]{1,7}";
+    private static final Pattern PATTERN = Pattern.compile(REGEX);
+    HashMap<String, ArrayList<LocalDate>> currentLicences = new HashMap<>();
+    private LocalDate today = LocalDate.now();
+
+    // Constructors
+    VisitorParking(){
+    }
+
+    public VisitorParking(String licence, LocalDate date)throws IllegalArgumentException{
+        if(date.isBefore(today))
+            throw new IllegalArgumentException();
+        licence = Parking.standardizeAndValidateLicence(licence);
+        currentLicences.computeIfAbsent(licence, k -> new ArrayList<>()).add(date);
+    }
+
+    public VisitorParking(String licence)throws IllegalArgumentException{
+        licence = Parking.standardizeAndValidateLicence(licence);
+        currentLicences.computeIfAbsent(licence, k -> new ArrayList<>()).add(today);
+    }
+
+    // Setters
+
+    // Getters
+    public ArrayList<String> getLicencesRegisteredForDate(){ 
+        ArrayList<String> licences = new ArrayList<>();
+        for(String key:currentLicences.keySet())
+            for(LocalDate checkDate:currentLicences.get(key))
+                if(checkDate == today)
+                    licences.add(key);
+        return licences; 
+    }
+    public ArrayList<String> getLicencesRegisteredForDate(LocalDate date){
+        ArrayList<String> licences = new ArrayList<>();
+        for(String key:currentLicences.keySet())
+            for(LocalDate checkDate:currentLicences.get(key))
+                if(checkDate == date)
+                    licences.add(key);
+        return licences; 
+    }
+
+    public ArrayList<LocalDate> getAllDaysLicenceIsRegistered(String licence){
+        return currentLicences.get(licence);
+    }
+
+    public ArrayList<LocalDate> getStartDaysLicenceIsRegistered(String licence){
+        ArrayList<LocalDate> startDates = new ArrayList<>();
+        for(int i = 0; i < currentLicences.get(licence).size(); i += 3){
+            startDates.add(currentLicences.get(licence).get(i));
+        }
+        Collections.sort(startDates);
+        return startDates;
+    }
+
+    // Methods
+    public void addVisitorReservation(String licence) {
+        addVisitorReservation(licence, today);
+    }
+    
+    public void addVisitorReservation(String licence, LocalDate date) throws IllegalArgumentException{
+        licence = Parking.standardizeAndValidateLicence(licence);
+        int numReservations = 0;
+        if(date.isBefore(today))
+            throw new IllegalArgumentException();
+        if(currentLicences.get(licence) == null){
+            currentLicences.computeIfAbsent(licence, k -> new ArrayList<>()).add(date);
+            currentLicences.computeIfAbsent(licence, k -> new ArrayList<>()).add(date.plusDays(1));
+            currentLicences.computeIfAbsent(licence, k -> new ArrayList<>()).add(date.plusDays(2));
+            return;
+        }
+        for(LocalDate checkDate:currentLicences.get(licence)){
+            if(date.isBefore(checkDate) && date.plusDays(2).isAfter(checkDate))
+                throw new IllegalArgumentException();
+                
+        }
+        for(String key:currentLicences.keySet())
+            for(LocalDate checkDate:currentLicences.get(key))
+                if(date == checkDate)
+                    numReservations ++;
+        System.out.println(numReservations);
+        if(numReservations > 2)
+            throw new IllegalArgumentException();
+        currentLicences.computeIfAbsent(licence, k -> new ArrayList<>()).add(date); 
+        currentLicences.computeIfAbsent(licence, k -> new ArrayList<>()).add(date.plusDays(1));
+        currentLicences.computeIfAbsent(licence, k -> new ArrayList<>()).add(date.plusDays(2));
+    }
+
+    public boolean licenceIsRegisteredForDate(String licence, LocalDate date) {
+        return currentLicences.get(licence).contains(date);
+    }
+
+    public boolean licenceIsRegisteredForDate(String licence) {
+        return currentLicences.get(licence).contains(today);
+    }
+}
